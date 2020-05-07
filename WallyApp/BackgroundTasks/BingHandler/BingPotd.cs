07403f7978ab6b68fe266279cf0437c9cd38cdc1
Ganesh Tiwari <ganesht049@gmail.com>
2020-05-07 20:30:46 +0530

    Add project files.

---
+++ WallyApp/BackgroundTasks/BingHandler/BingPotd.cs
@@ -0,0 +1,20 @@
+﻿using EasyHttp.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WallyApp.BackgroundTasks
+{
+    class BingPotd
+    {
+        async public void GetImage()
+        {
+            var httpClient = new HttpClient();
+            var response = httpClient.Get(StaticData.BING_POTD_URL);
+            Debug.WriteLine("Request came back " + response.RawText);
+        }
+    }
+}
---
+++ WallyApp/BackgroundTasks/BingHandler/StaticData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WallyApp
+{
+    class StaticData
+    {
+        private static string bING_POTD_URL = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US";
+
+        public static string BING_POTD_URL { get => bING_POTD_URL; }
+    }
+}
