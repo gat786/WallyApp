a9cc2f6127f47770a82709b3830b68b2a1e576f7
Ganesh Tiwari <ganesht049@gmail.com>
2020-05-18 20:29:20 +0530

    added feature to set wallpaper

--- WallyApp/BackgroundTasks/WallpaperHandler.cs
+++ WallyApp/BackgroundTasks/WallpaperHandler.cs
@@ -1,23 +1,39 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WallyApp.BackgroundTasks.BingHandler;
+using Windows.Storage;
 using Windows.System.UserProfile;
 
 namespace WallyApp.BackgroundTasks
 {
     class WallpaperHandler
     {
-        Boolean setWallpaper()
+        public async Task<bool> setWallpaper(StorageFile imageFile)
         {
             bool success = false;
 
+            Debug.WriteLine(UserProfilePersonalizationSettings.Current.GetType());
+            Debug.WriteLine(UserProfilePersonalizationSettings.Current.ToString());
             if (UserProfilePersonalizationSettings.IsSupported())
             {
-
+                UserProfilePersonalizationSettings profileSettings = UserProfilePersonalizationSettings.Current;
+                success = await profileSettings.TrySetWallpaperImageAsync(imageFile);
             }
-            return true;
+            return success;
+        }
+
+        public async void saveImageToFile(Stream imageStream, StorageFile fileToWrite)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageStream.CopyTo(ms);
+            await FileIO.WriteBytesAsync(fileToWrite,
+                                         ms.GetBuffer());
         }
     }
 }
