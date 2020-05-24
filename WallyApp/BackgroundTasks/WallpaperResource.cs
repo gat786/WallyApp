de05fa783fd18d0b9d4e94bbb1a2b91d7bab570f
Ganesh Tiwari <ganesht049@gmail.com>
2020-05-24 22:50:41 +0530

    created a interface for handlers to folloow
    The new interface will lead to a newer way of doing things and which will needed to be used by handlers inorder to work well with the main ui of the app

--- WallyApp/BackgroundTasks/WallpaperResource.cs
+++ WallyApp/BackgroundTasks/WallpaperResource.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml.Media.Imaging;
 
 namespace WallyApp.BackgroundTasks
 {
-    interface WallpaperResource
+    public interface WallpaperResource
     {
-        bool getJsonResponse();
-        BitmapImage GetBitmapImage();
-        byte[] getImageBuffer();
+        Task<string> GetJsonResponse();
+        Task<Stream> GetStreamOfImage();
+        Task<BitmapImage> GetBitmapImage();
+        Task<byte[]> GetImageBuffer();
+        string GetFileName();
     }
 }
